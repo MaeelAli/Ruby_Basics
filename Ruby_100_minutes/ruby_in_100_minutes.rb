c134591ede7a20c0560a4bc8edb8c0d7c1793b01
Ismaeel <maeelali92@gmail.com>
2016-01-24 19:11:41 +0000

    Started Ruby in 100 minutes course. Parts 1 and 2 finished

---
+++ Ruby_100_minutes/ruby_in_100_minutes.rb
@@ -0,0 +1,32 @@
+
+#=begin ############################################################
+# 1. Instructions and Interpreters
+# Running Ruby from the command line
+# Hello World Example
+
+
+class Sample
+  def hello
+    puts "Hello, world"
+  end
+end
+
+s = Sample.new
+s.hello
+#=end #############################################################
+
+#=begin ###########################################################
+# 2. Variables
+
+# Creating and Assigning a Variable
+a = 5
+puts a
+
+#Right Side First
+b = 10 + 5
+puts b
+
+#Flexible Typing
+c = 20
+c = "hello"
+puts c
